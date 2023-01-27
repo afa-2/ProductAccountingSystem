51afeebe93fde330d256a397fcbb40aca2249499
afa-spb <skorobey.spb@mail.ru>
2023-01-27 16:37:46 +0400

    changed instal docker file

--- install_docker.sh
+++ install_docker.sh
@@ -6,4 +6,3 @@ sudo sh /get-docker.sh
 sudo curl -L "https://github.com/docker/compose/releases/download/1.25.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo usermod -aG docker $(whoami)
 sudo chmod +x /usr/local/bin/docker-compose
-
